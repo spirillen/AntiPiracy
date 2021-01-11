8681db8831feaac1050883fceb8d86b4c20531ad
Spirillen <pdns@protonmail.com>
2021-01-11 05:55:20 +0100

    AntiPiracy list updated: 11. January 2021 05:55 CET

--- Blocklist/update.sh
+++ Blocklist/update.sh
@@ -15,8 +15,16 @@ wget -qO- 'http://list.iblocklist.com/?list=uwnukjqktoggdknzrhgh&fileformat=p2p&
 
 wget -qO - 'https://silo.glasz.org/antip2p.list.gz' | gunzip - > 'antip2p.p2p'
 
+wget -qO- 'https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/p2p/extra.txt' | grep -vE '^(#|$)' >> WindowsSpyBlocker.txt
+
+wget -qO- 'https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/p2p/spy.txt' | grep -vE '^(#|$)' >> WindowsSpyBlocker.txt
+
+wget -qO- 'https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/p2p/update.txt' | grep -vE '^(#|$)' >> WindowsSpyBlocker.txt
+
 ## Let's combine and sort the list to our own
-cat 'Bluetack_level1.p2p' 'Bluetack_level2.p2p' 'Bluetack_level3.p2p' | sort -u > "${ROOT_DIR}/docs/torrent.p2p"
+cat 'antip2p.p2p' 'Bluetack_level1.p2p' 'Bluetack_level2.p2p' \
+    'Bluetack_level3.p2p' 'WindowsSpyBlocker.txt' | \
+    sort -u > ${ROOT_DIR}/public/torrent.p2p
 
 ## Copy to public
 cd "${ROOT_DIR}/docs/"
@@ -29,6 +37,7 @@ gzip -k -9 -f torrent.p2p
 rm "${ROOT_DIR}/Blocklist/Bluetack_level1.p2p" \
   "${ROOT_DIR}/Blocklist/Bluetack_level2.p2p" \
   "${ROOT_DIR}/Blocklist/Bluetack_level3.p2p" \
-  "${ROOT_DIR}/Blocklist/antip2p.p2p"
+  "${ROOT_DIR}/Blocklist/antip2p.p2p" \
+  "${ROOT_DIR}/Blocklist/WindowsSpyBlocker.txt"
 
 cd "${ROOT_DIR}"
