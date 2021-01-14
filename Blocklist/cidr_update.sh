0fc89fd1283cf06fbe70b454086385acf4f4317b
Spirillen <pdns@protonmail.com>
2021-01-14 06:14:26 +0100

    AntiPiracy list updated: 14. January 2021 06:14 CET

--- Blocklist/cidr_update.sh
+++ Blocklist/cidr_update.sh
@@ -16,8 +16,6 @@ wget -qO level3.gz 'http://list.iblocklist.com/?list=uwnukjqktoggdknzrhgh&filefo
 #cat 'Bluetack_level*.cidr' 'torrent.cidr' | sort -u > 'torrent.cidr.tmp' \
 #  && mv 'torrent.cidr.tmp' 'torrent.cidr'
 
-$(which python3.9) "ip2cidr.py"
-
 wget -qO - 'https://silo.glasz.org/antip2p.list.gz' | gunzip - > 'antip2p.cidr.tmp'
 
 cut -d ':' -f 2 'antip2p.cidr.tmp' > 'antip2p.txt'
@@ -25,13 +23,15 @@ cut -d ':' -f 2 'antip2p.cidr.tmp' > 'antip2p.txt'
 ## Let's combine and sort the list to our own
 #cat 'Bluetack_level1.cidr' 'Bluetack_level2.cidr' 'Bluetack_level3.cidr' 'antip2p.txt' | sort -u > "${ROOT_DIR}/docs/torrent.txt"
 
+cd "${ROOT_DIR}"
+$(which python3.9) "Blocklist/ip2cidr.py"
+
+
 ## Cleanup
-rm "${ROOT_DIR}/Blocklist/Bluetack_level1.cidr" \
-  "${ROOT_DIR}/Blocklist/Bluetack_level2.cidr" \
-  "${ROOT_DIR}/Blocklist/Bluetack_level3.cidr" \
+rm "${ROOT_DIR}/Blocklist/Bluetack_level1.gzip" \
+  "${ROOT_DIR}/Blocklist/Bluetack_level2.gzip" \
+  "${ROOT_DIR}/Blocklist/Bluetack_level3.gzip" \
   "${ROOT_DIR}/Blocklist/antip2p.cidr.tmp" \
   "${ROOT_DIR}/Blocklist/antip2p.txt"
-
-cd "${ROOT_DIR}"
 
 #exec "${ROOT_DIR}/Blocklist/update.sh" & exit 0
