0f0935ed9c8794f18f75cfec14381656dbea887a
Spirillen <pdns@protonmail.com>
2021-01-14 05:59:25 +0100

    AntiPiracy list updated: 14. January 2021 05:59 CET

--- Blocklist/ip2cidr.py
+++ Blocklist/ip2cidr.py
@@ -36,25 +36,25 @@ target_url = 'http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw&fileformat=c
 cidr = [ ]
 
 
-with gzip.open('Blocklist/cidr.gz', 'rb') as ip:
+with gzip.open('cidr.gz', 'rb') as ip:
         with io.TextIOWrapper(ip, encoding='utf-8') as decoder:
             # Let's read the content using read()
             content = decoder.read()
             cidr.append(content.rstrip('\n'))
 
-with gzip.open('Blocklist/level1.gz', 'rb') as ip2:
+with gzip.open('level1.gz', 'rb') as ip2:
         with io.TextIOWrapper(ip2, encoding='utf-8') as decoder2:
             # Let's read the content using read()
             content1 = decoder2.read()
             cidr.append(content1.rstrip('\n'))
 
-with gzip.open('Blocklist/level2.gz', 'rb') as ip3:
+with gzip.open('level2.gz', 'rb') as ip3:
         with io.TextIOWrapper(ip3, encoding='utf-8') as decoder3:
             # Let's read the content using read()
             content2 = decoder3.read()
             cidr.append(content2.rstrip('\n'))
 
-with gzip.open('Blocklist/level3.gz', 'rb') as ip4:
+with gzip.open('level3.gz', 'rb') as ip4:
         with io.TextIOWrapper(ip4, encoding='utf-8') as decoder4:
             # Let's read the content using read()
             content3 = decoder4.read()
@@ -64,7 +64,7 @@ res = list(OrderedDict.fromkeys(cidr))
 #for element in res:
 #    print(str(element))
 
-with open('Blocklist/torrent.cidr', 'w') as new_file:
+with open('torrent.cidr', 'w') as new_file:
         for element in res:
             new_file.write(str(element))
 
